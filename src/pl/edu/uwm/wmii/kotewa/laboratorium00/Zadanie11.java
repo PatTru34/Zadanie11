8731ebe17737651f2fccd9dba3dd1ce5c684d5ed
Patryk Truchan <162391@student.uwm.edu.pl>
2021-10-12 10:08:37 +0200

    Initial commit

---
+++ src/pl/edu/uwm/wmii/kotewa/laboratorium00/Zadanie11.java
@@ -0,0 +1,11 @@
+package pl.edu.uwm.wmii.kotewa.laboratorium00;
+
+public class Zadanie11 {
+
+    public static void main(String[] args) {
+        System .out. println ("Wiersz pt. Skarga Zmiętego");
+        System .out. println ("Autor: Jan Izydor Sztaudynger");
+        System .out. println ("Życie mnie");
+        System .out. println ("Mnie!");
+    }
+}
